f70659e62dce568338acadc34920f67fda3d4ce8
rehan anwar <anwarrehan2587@gmail.comm>
2026-05-14 22:10:58 +0530

    week_1 file added

---
+++ week_1/program_1.py
@@ -0,0 +1,40 @@
+#data of my self
+name="rehan" #my name
+age=16 #my age
+height=5.7 #my height
+relationship="self" #relationship self
+print("name:",name)
+print("age:",age)
+print("height:",height)
+print("relation:",relationship)
+print("_________________________________________________________")
+#data of my brother
+name1="zeeshan" #name of my brother
+age1=20 #age of my brother
+height1=5.7 #height of my brother
+relationship1="brother" #relationship brother
+print("name:",name1)
+print("age:",age1)
+print("height:",height1)
+print("relation:",relationship1)
+print("___________________________________________________________")
+#data of my sister
+name2="bushra" #name of my sister
+age2=22 #age of my sister
+height2=5.3 #height of my sister
+relationship2="sister" #relationship sister
+print("name:",name2)
+print("age:",age2)
+print("height:",height2)
+print("relation:",relationship2)
+print("______________________________________________________________")
+#data of my father
+name3="khalid" #name of my father
+age3=48 #age of my father
+height3=5.6 #height of my father
+relationship3="father" #relationship father
+print("name:",name3)
+print("age:",age3)
+print("height:",height3)
+print("relation:",relationship3)
+print("________________________________________________________________-")
